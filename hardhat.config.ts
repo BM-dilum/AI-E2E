3f3640c9a8bc3fbcfc98694ce0b372e82b124447
Developer <dev@example.com>
2026-03-24 13:09:07 +1100

    feat: add hello world smart contract with tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,10 +2,13 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
-  networks: {
-    hardhat: {
-      allowUnlimitedContractSize: true,
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
     },
   },
 };
